987733bfebbc90f16f1ed858a2e0e9bf9304c9f6
Gustavo Domingueti <gustav.s.domingueti@gmail.com>
2021-01-10 01:42:53 -0300

    :zap: Remove stagger

--- utils.js
+++ utils.js
@@ -6,13 +6,12 @@ function initConffeti(element) {
     const config = {
         spread: 150,
         startVelocity: '100',
-        elementCount: '300',
+        elementCount: '250',
         width: '20px',
         height: '20px',
         perspective: '500px',
         colors: ['var(--primary)', 'var(--primary-2)', 'var(--secondary)', 'var(--white)'],
         duration: '7000',
-        stagger: 1,
     };
     confetti(element, config);
 }
