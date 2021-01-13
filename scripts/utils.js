755e947edd292da049b58fec921213634221ac69
Gustavo Domingueti <gustav.s.domingueti@gmail.com>
2021-01-12 23:47:06 -0300

    :zap: Adjustments

--- scripts/utils.js
+++ scripts/utils.js
@@ -14,13 +14,12 @@ function initConffeti(element) {
     const config = {
         spread: 150,
         startVelocity: '100',
-        elementCount: '300',
+        elementCount: '250',
         width: '1.5rem',
         height: '1.5rem',
         perspective: '500px',
         colors: ['var(--primary)', 'var(--primary-2)', 'var(--secondary)', 'var(--white)'],
         duration: '6000',
-        dragFriction: 0.125,
     };
     confetti(element, config);
 }
