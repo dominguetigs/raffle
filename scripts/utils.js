08e79a46f00244f5020f0673c95ae63268319330
Gustavo Domingueti <gustav.s.domingueti@gmail.com>
2021-01-12 23:43:26 -0300

    :zap: Add dragFriction in conffeti configuration

--- scripts/utils.js
+++ scripts/utils.js
@@ -14,12 +14,13 @@ function initConffeti(element) {
     const config = {
         spread: 150,
         startVelocity: '100',
-        elementCount: '250',
-        width: '25px',
-        height: '25px',
+        elementCount: '300',
+        width: '1.5rem',
+        height: '1.5rem',
         perspective: '500px',
         colors: ['var(--primary)', 'var(--primary-2)', 'var(--secondary)', 'var(--white)'],
-        duration: '5500',
+        duration: '6000',
+        dragFriction: 0.125,
     };
     confetti(element, config);
 }
